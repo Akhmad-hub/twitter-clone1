79572e58e88f135f4d6bf962b0629f3c78c6e77b
Safsafi <safiiakhmad7@gmail.com>
2024-05-16 11:21:38 +0700

    3- payload limit

--- frontEnd/src/pages/home/CreatePostPages.jsx
+++ frontEnd/src/pages/home/CreatePostPages.jsx
@@ -62,7 +62,7 @@ const CreatePostPages = () => {
       </div>
       <form className="flex flex-col gap-2 w-full" onSubmit={handleSubmit}>
         <textarea
-          className="textarea w-full p-0 text-lg resize-none border-none focus:outline-none  border-gray-800"
+          className="textarea w-full p-0 text-lg resize-none border-none focus:outline-none  border-gray-800 h-auto"
           placeholder="What is happening?!"
           value={text}
           onChange={(e) => setText(e.target.value)}
